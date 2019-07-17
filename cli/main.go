fd18f234655fcb0b1384a532c28242fdc1d631f8
DACrowder <dave.alex.crowder@gmail.com>
2019-07-17 16:15:33 -0600

    Add tool for querying the reports db

---
+++ cli/main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"golang.org/x/oauth2"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const baseurl = "http://127.0.0.1:3333"
+
+var (
+	key, gid, slvl, ghUser, ghToken, jwt string
+	stype                                = -1
+	delReq                               = false
+	err                                  error
+)
+
+func init() {
+	flag.Usage = func() {
+		flag.PrintDefaults()
+		fmt.Println("Precedence of report qualifiers: key > group > severity = type")
+		fmt.Println("i.e. if group and severity given, severity will be ignored")
+	}
+	// short flags
+	flag.StringVar(&ghUser, "u", "", "your github username (for token requests)")
+	flag.StringVar(&ghToken, "o", "", "your github oauth token (for token requests)")
+	flag.StringVar(&jwt, "j", "", "the report server auth jwt to use")
+	flag.StringVar(&key, "k", "", "the report key to lookup")
+	flag.StringVar(&gid, "g", "", "the report group id to lookup")
+	flag.StringVar(&slvl, "s", "", "the report severity string (bug | unknown | crash)")
+	flag.IntVar(&stype, "t", -1, "the report type (severity level in numeric form: 0|1|2)")
+
+	// corresponding long flags
+	flag.StringVar(&ghUser, "user", "", "your github username (for token requests)")
+	flag.StringVar(&ghToken, "oauth", "", "your github oauth token (for token requests)")
+	flag.StringVar(&jwt, "jwt", "", "the report server auth jwt to use")
+	flag.StringVar(&key, "key", "", "the report key to lookup")
+	flag.StringVar(&gid, "gid", "", "the report group id to lookup")
+	flag.StringVar(&slvl, "severity", "", "the report severity string (bug | unknown | crash)")
+	flag.IntVar(&stype, "type", -1, "the report type (severity level in numeric form: 0|1|2)")
+	flag.BoolVar(&delReq, "Delete", false, "when set, the reports found will be deleted")
+
+	flag.Parse()
+}
+
+func main() {
+	if (ghUser == "" || ghToken == "") && jwt == "" {
+		_, _ = fmt.Fprint(os.Stderr, "Cannot make request: no auth token (nor github for retrieval) provided\n")
+		flag.Usage()
+		os.Exit(http.StatusUnauthorized)
+		return
+	}
+
+	if jwt == "" {
+		if jwt, err = getJWT(); err != nil {
+			_ = fmt.Errorf("could not retrieve auth jwt: %v\n", err.Error())
+			os.Exit(1)
+			return
+		}
+		fmt.Printf("JWT: %v\n", jwt)
+	}
+
+	tc := oauth2.NewClient(
+		context.Background(),
+		oauth2.StaticTokenSource(&oauth2.Token{AccessToken: jwt}),
+	)
+	b, _ := body(map[string]interface{}{}) // did not give data thus no error possible
+	req, err := http.NewRequest(method(), url(), b)
+	if err != nil {
+		_ = fmt.Errorf("failed to create request: %v\n", err.Error())
+		os.Exit(2)
+		return
+	}
+
+	resp, err := tc.Do(req)
+	if err != nil {
+		_ = fmt.Errorf("request failed: %v\n", err.Error())
+		os.Exit(3)
+		return
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Printf("failed to close response body: %v", err.Error())
+		}
+	}()
+
+	data := map[string]interface{}{}
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		_ = fmt.Errorf("Could not decode response from server: %v\n", err.Error())
+		os.Exit(4)
+		return
+	}
+
+}
+
+// getJWT from server using github credentials (uname + oauth2 token)
+func getJWT() (string, error) {
+	body, err := body(map[string]interface{}{
+		"ghUser":  ghUser,
+		"ghToken": ghToken,
+	})
+	if err != nil {
+		return "", err
+	}
+	resp, err := http.Post(baseurl+"/token", "application/json", body)
+	if err != nil {
+		return "", err
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(b)), nil
+}
+
+func body(data map[string]interface{}) (io.Reader, error) {
+	b := new(bytes.Buffer)
+	if len(data) == 0 {
+		return b, nil
+	}
+	err := json.NewEncoder(b).Encode(data)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func method() string {
+	if delReq {
+		return http.MethodDelete
+	}
+	return http.MethodGet
+}
+
+func url() string {
+	url := baseurl + "/report"
+	if key != "" {
+		url += "/key/" + key + "/"
+	} else if gid != "" {
+		url += "/group/" + gid + "/"
+	} else if slvl != "" {
+		url += "/severity/" + slvl + "/"
+	} else if stype >= 0 {
+		url += "/severity/" + strconv.Itoa(stype) + "/"
+	} else {
+		url += "/"
+	}
+	return url
+}
